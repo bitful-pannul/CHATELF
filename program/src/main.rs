6c6d794878df44729a52358bb8d30acbfd8410c2
dr-frmr <docterformer@protonmail.com>
2024-02-28 12:05:24 -0300

    working

--- program/src/main.rs
+++ program/src/main.rs
@@ -9,10 +9,20 @@ use std::collections::HashMap;
 sp1_zkvm::entrypoint!(main);
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ChatMessage {
+struct ChatMessage {
     pub sender: String,
     pub text: String,
     pub timestamp: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+enum ProofResult {
+    NotFound,
+    Found {
+        checkpoint_timestamp: u64,
+        sender: String,
+        text: String,
+    },
 }
 
 type Checkpoints = HashMap<u64, (Vec<u8>, Vec<ChatMessage>)>;
@@ -21,20 +31,27 @@ pub fn main() {
     let checkpoints = sp1_zkvm::io::read::<Checkpoints>();
     let search_string = sp1_zkvm::io::read::<String>();
 
-    let mut result: Option<(ChatMessage, u64)> = None;
+    let mut result = ProofResult::NotFound;
 
     for (checkpoint_timestamp, (hash, messages)) in checkpoints.into_iter() {
-        let hasher = Sha256::new();
-
-        assert!(hasher.finalize().to_vec() == hash);
+        // let mut hasher = Sha256::new();
+        // hasher.update(serde_json::to_vec(&messages).unwrap());
+        // assert!(hasher.finalize().to_vec() == hash);
 
         for message in messages {
             if message.text.contains(&search_string) {
-                result = Some((message, checkpoint_timestamp));
+                result = ProofResult::Found {
+                    checkpoint_timestamp,
+                    sender: message.sender,
+                    text: message.text,
+                };
                 break;
             }
         }
+        if result != ProofResult::NotFound {
+            break;
+        }
     }
 
-    sp1_zkvm::io::write(&true);
+    sp1_zkvm::io::write(&result);
 }
